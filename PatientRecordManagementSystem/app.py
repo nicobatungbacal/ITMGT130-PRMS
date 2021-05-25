f40546ff37b1137683fca748a5c5a4c64ae5e229
nicobatungbacal <84030423+nicobatungbacal@users.noreply.github.com>
2021-05-25 12:22:03 +0800

    Update

--- PatientRecordManagementSystem/app.py
+++ PatientRecordManagementSystem/app.py
@@ -2,5 +2,17 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return 'Index Page. Place Home Page contents here.'
+
+@app.route('/services')
+def products():
+    return 'Services Page. Place Services Page contents here.'
+
+@app.route('/booking')
+def branches():
+    return 'Booking Page. Place Booking Page contents here.'
+
+@app.route('/doctors')
+def aboutus():
+    return 'Doctors Page. Place Doctors Page contents here.'
